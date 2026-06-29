b704cc1d58cbe7fbad1456005eef2405b421a336
Sagar D <dharaglkarsagar@gmail.com>
2026-06-29 17:11:40 +0530

    chore: update base URL in api.dart and increment version to 1.0.7+8 in pubspec.yaml

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -5,7 +5,7 @@ import 'storage.dart';
 import 'providers/connectivity_provider.dart';
 
 const String baseUrl = 'https://vittam.vengurlatech.com/api';
-// const String baseUrl = 'http://192.168.1.11:5000/api';
+// const String baseUrl = 'http://192.168.1.4:5000/api';
 const String _genericApiErrorMessage = 'Something went wrong';
 
 String sanitizeUiErrorMessage(Object? error, {String fallback = _genericApiErrorMessage}) {
