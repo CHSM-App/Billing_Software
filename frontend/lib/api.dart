f2f61d1347848c95a9795196a334465158bec439
Sagar D <dharaglkarsagar@gmail.com>
2026-08-03 12:45:56 +0530

    fix: update base URL to production API endpoint

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -5,8 +5,8 @@ import 'package:http/http.dart' as http;
 import 'storage.dart';
 import 'providers/connectivity_provider.dart';
 
-// const String baseUrl = 'https://vittam.vengurlatech.com/api';
-const String baseUrl = 'http://192.168.1.14:5000/api';
+const String baseUrl = 'https://vittam.vengurlatech.com/api';
+// const String baseUrl = 'http://192.168.1.14:5000/api';
 const String _genericApiErrorMessage = 'Something went wrong';
 
 String sanitizeUiErrorMessage(Object? error, {String fallback = _genericApiErrorMessage}) {
