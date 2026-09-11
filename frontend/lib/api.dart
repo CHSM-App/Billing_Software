a45be1fd0d57f1879b4024355e9f33d0c06faa1d
Sagar D <dharaglkarsagar@gmail.com>
2026-09-11 11:07:24 +0530

    feat: Remove commented-out local API URL for clarity and maintainability

--- frontend/lib/api.dart
+++ frontend/lib/api.dart
@@ -9,7 +9,6 @@ import 'providers/connectivity_provider.dart';
 
 
 const String baseUrl = 'https://vittam.vengurlatech.com/api';
-// const String baseUrl = 'http://192.168.1.5:5000/api';
 
 const String _genericApiErrorMessage = 'Something went wrong';
           
